572faa682f909cf86fc0acfef20853c453ef12bf
DmV <DmV@mail.com>
2024-09-28 19:06:21 +0300

    Add Task2

---
+++ Lesson1/Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//Console.WriteLine("Hello, World!");
+
+int value = 100;
+double d = 5.55;
+char c = '!';
+bool b = true;
+
+Console.WriteLine(value);
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
