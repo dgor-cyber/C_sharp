086d73379841eeaf7a2734a0af73d8d9f904b444
DmV <DmV@mail.com>
2024-09-29 22:59:34 +0300

    Add Homework2

---
+++ Homework2/Homework2_Task1/Homework2_Task1.cs
@@ -0,0 +1,22 @@
+﻿/*
+Задача 10: 
+Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1
+*/
+
+using System;
+
+while (true)
+{
+    Console.Write("Введите целое трехзначное число: ");
+    if (int.TryParse(Console.ReadLine(), out int num) && num < 1000 && num > 99)
+    {
+        int secondDigit = num / 10 % 10;
+        Console.WriteLine($"2-я цифра числа - {secondDigit}");
+        break;
+    }
+    Console.WriteLine("Ошибка ввода! Введите целое трехзначное число!");
+}
---
+++ Homework2/Homework2_Task2/Homework2_Task2.cs
@@ -0,0 +1,29 @@
+﻿/*
+Задача 13: 
+Напишите программу, которая выводит третью цифру заданного числа 
+или сообщает, что третьей цифры нет.
+645 -> 5
+78 -> третьей цифры нет
+32679 -> 6
+*/
+
+using System;
+
+while (true)
+{
+    Console.Write("Введите целое число: ");
+    if (int.TryParse(Console.ReadLine(), out int num))
+    {
+        if (num < 100 && num > -100)
+        {
+            Console.WriteLine("третьей цифры нет");
+            break;
+        }
+        else
+        {
+            Console.WriteLine($"3-я цифра числа - {num.ToString()[2]}");
+            break;
+        }
+    }
+    Console.WriteLine("Ошибка ввода! Введите целое число!");
+}
